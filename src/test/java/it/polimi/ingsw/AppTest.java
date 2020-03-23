d0f07d0ceb83aa1c416c907ee81770412d69f953
AlfonsoCatelliUni <alfonso.catelli@mail.polimi.it>
2020-03-23 18:20:27 +0100

    new commit to verify that .gitignore works well, new empty ;) Game class

--- src/test/java/it/polimi/ingsw/AppTest.java
+++ src/test/java/it/polimi/ingsw/AppTest.java
@@ -3,17 +3,14 @@ package it.polimi.ingsw;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
     public void shouldAnswerWithTrue() {
         //test 1
         assertTrue( true );
     }
+
+
 }

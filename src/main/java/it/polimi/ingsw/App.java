c113975af1046f9464c9296aa4f514dfd8761f72
AlfonsoCatelliUni <alfonso.catelli@mail.polimi.it>
2020-03-23 18:27:40 +0100

    moved the UML files into Deliverables directory

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -1,13 +1,9 @@
 package it.polimi.ingsw;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Bitch!" );
     }
 }

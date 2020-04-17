b913433f5aab93a2b5ebefaa547fc01864ffcafc
AlfonsoCatelliUni <alfonso.catelli@mail.polimi.it>
2020-04-17 18:04:24 +0200

    All State classes

--- src/main/java/it/polimi/ingsw/model/Player/State/FinishState.java
+++ src/main/java/it/polimi/ingsw/model/Player/State/FinishState.java
@@ -1,4 +1,39 @@
 package it.polimi.ingsw.model.Player.State;
 
-public class FinishState {
+import it.polimi.ingsw.model.Actions.Action;
+import it.polimi.ingsw.model.Actions.FinishAction;
+import it.polimi.ingsw.model.Board.Board;
+import it.polimi.ingsw.model.Player.Pawn;
+import it.polimi.ingsw.model.Player.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FinishState implements StateInterface {
+
+    //TODO : cambiare in effect quando gli altri committeranno
+    private Player player;
+
+
+    private List<Action> actions;
+
+
+    // ======================================================================================
+
+
+    public FinishState(Player player) {
+        this.player = player;
+
+        this.actions = new ArrayList<>();
+        this.actions.add(new FinishAction());
+    }
+
+
+    // ======================================================================================
+
+
+    public List<Action> checkPossibleActions(Board gameBoard, Pawn designatedPawn) {
+        return new ArrayList<>(actions);
+    }
+
 }
--- src/main/java/it/polimi/ingsw/model/Player/State/MoveState.java
+++ src/main/java/it/polimi/ingsw/model/Player/State/MoveState.java
@@ -1,4 +1,48 @@
 package it.polimi.ingsw.model.Player.State;
 
-public class MoveState {
+import it.polimi.ingsw.model.Actions.Action;
+import it.polimi.ingsw.model.Actions.MoveAction;
+import it.polimi.ingsw.model.Board.Board;
+import it.polimi.ingsw.model.Player.Pawn;
+import it.polimi.ingsw.model.Player.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MoveState implements StateInterface {
+
+
+    private final Player player;
+
+
+    private final List<Action> actions;
+
+
+    // ======================================================================================
+
+
+    public MoveState(Player player) {
+        this.player = player;
+
+        this.actions = new ArrayList<>();
+        this.actions.add(new MoveAction());
+    }
+
+
+    // ======================================================================================
+
+
+    public List<Action> checkPossibleActions(Board gameBoard, Pawn designatedPawn) {
+
+        List<Action> possibleActions = new ArrayList<>(actions);
+
+        //TODO : cambiare in effect quando committeranno gli altri
+        if(player.wherePawnCanMove(gameBoard, designatedPawn).size() == 0) {
+            possibleActions.removeIf(a -> a.getClass().equals(MoveAction.class));
+        }
+
+        return possibleActions;
+    }
+
+
 }

afbfb609464e79b819b47118293cc5696d840ef3
GianmarcoAulicino <gianmarco.aulicino@mail.polimi.it>
2020-04-18 01:08:19 +0200

    Fixed BuildBeforeEffect and added BuildNotOnSameCellEffectTest

--- src/test/java/it/polimi/ingsw/model/Player/Effect/BuildBeforeEffectTest.java
+++ src/test/java/it/polimi/ingsw/model/Player/Effect/BuildBeforeEffectTest.java
@@ -6,6 +6,10 @@ import it.polimi.ingsw.model.Board.Building;
 import it.polimi.ingsw.model.Board.Cell;
 import it.polimi.ingsw.model.Color;
 import it.polimi.ingsw.model.Player.Card;
+import it.polimi.ingsw.model.Player.State.BuildState;
+import it.polimi.ingsw.model.Player.State.FinishState;
+import it.polimi.ingsw.model.Player.State.MoveAndBuildState;
+import it.polimi.ingsw.model.Player.State.MoveState;
 import it.polimi.ingsw.model.Sex;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -63,8 +67,17 @@ class BuildBeforeEffectTest {
         /* case when the player can build before moving, so NumBuild increases by 1, and I have to reset it to 1,
         after that I set the flag hasBuiltBefore */
 
+        assertEquals(MoveAndBuildState.class, player3.getEffect().getState().getClass());
 
         player3.build(player3.getPawns().get(0), gameBoard.getCell(3,3), 1, buildings);
-        
+
+        assertEquals(MoveState.class, player3.getEffect().getState().getClass());
+
+        player3.getEffect().changeState(new BuildState(player3.getEffect()));
+
+        player3.build(player3.getPawns().get(0), gameBoard.getCell(3,3), 2, buildings);
+
+        assertEquals(FinishState.class, player3.getEffect().getState().getClass());
+
     }
 }

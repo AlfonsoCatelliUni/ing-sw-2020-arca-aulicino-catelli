f4d3723c64228bac770aa7cc0e1b3869a389d5b7
AlfonsoCatelliUni <alfonso.catelli@mail.polimi.it>
2020-04-18 02:05:43 +0200

    some javadocs before deleting the Static Decorator

--- src/main/java/it/polimi/ingsw/model/Player/Effect/BasicEffect.java
+++ src/main/java/it/polimi/ingsw/model/Player/Effect/BasicEffect.java
@@ -18,23 +18,29 @@ import it.polimi.ingsw.model.Player.State.StateInterface;
 import java.util.ArrayList;
 import java.util.List;
 
+
+/**
+ * this is the basic effect class, with the standard method,
+ * this class will be decorated according to the chosen {@link it.polimi.ingsw.model.Player.Card}
+ */
 public class BasicEffect implements Effect {
 
 
     private StateInterface state;
 
 
-    // ======================================================================================
-    // MARK : Constructor Section
+    // MARK : Constructor Section ======================================================================================
 
 
+    /**
+     * base constructor
+     */
     public BasicEffect() {
         this.state = new MoveState(this);
     }
 
 
-    // ======================================================================================
-    // MARK : Getter Section
+    // MARK : Getter Section ======================================================================================
 
 
     @Override
@@ -43,67 +49,111 @@ public class BasicEffect implements Effect {
     }
 
 
-    //TODO : controllare ?!
     @Override
     public Effect getEffect() {
         return this;
     }
 
 
-    // ======================================================================================
-    // MARK : Setter Section
+    // MARK : Setter Section ======================================================================================
 
 
+    /**
+     * this method is used to change the state of the player based
+     * the next move that he can do
+     * @param state the new state that I want to set
+     */
     @Override
     public void changeState(StateInterface state) {
         this.state = state;
     }
 
 
-    // ======================================================================================
-    // MARK : Possibilities Control Section
+    // MARK : Possibilities Control Section ======================================================================================
 
 
+    /**
+     * return which are the possible actions that i can do with the selected pawn
+     * if i can't do any move with the pawn i lose the game, it's the basic possible action,
+     * it will by decorated according with the selected God card
+     * @param gameBoard the board of the game
+     * @param designatedPawn the pawn selected to generate the possible actions
+     * @return the list of possible actions
+     */
     @Override
     public List<Action> getPossibleActions(Board gameBoard, Pawn designatedPawn) {
         return state.checkPossibleActions(gameBoard, designatedPawn);
     }
 
 
+    /**
+     * the standard method that return the cells where a pawn can move
+     * @param gameBoard the game board where the pawn have to move on
+     * @param designatedPawn the pawn that's designated to move
+     * @return the list of cells available to be move
+     */
     @Override
     public List<Cell> wherePawnCanMove(Board gameBoard, Pawn designatedPawn) {
         return gameBoard.getCellAvailableToMove( designatedPawn );
     }
 
 
+    /**
+     * the standard method that return the cells where a pawn can build
+     * @param gameBoard the game board where the pawn have to build on
+     * @param designatedPawn the pawn that's designated to build
+     * @return the list of cells available to be built
+     */
     @Override
     public List<Cell> wherePawnCanBuild(Board gameBoard, Pawn designatedPawn) {
         return gameBoard.getCellAvailableToBuild( designatedPawn );
     }
 
 
+    /**
+     * this method returns the list of possible building on the designatedCell
+     * @param designatedCell the cell that i've chosen to build on
+     * @return the list of possible building on the designated cell
+     */
     @Override
     public List<Building> getPossibleBuildingOnCell(Board gameBoard, Cell designatedCell) {
         return gameBoard.getPossibleBuildingOnCell( designatedCell );
     }
 
 
+    /**
+     * the standard method that return the cells where a pawn can be forced to go
+     * @param gameBoard the game board
+     * @param designatedPawn the pawn that I want to force
+     * @return the list of cells
+     */
     @Override
     public List<Cell> wherePawnCanForce(Board gameBoard, Pawn designatedPawn) {
         throw new RuntimeException("You're not enough strong to use the Force, Young Padawan!");
     }
 
 
+    /**
+     * the standard method that return the cells where I can destroy a block (not a dome)
+     * @param gameBoard the game board
+     * @param designatedPawn the pawn that want to destroy a block
+     * @return the list of cells
+     */
     @Override
     public List<Cell> wherePawnCanDestroy(Board gameBoard, Pawn designatedPawn) {
         throw new RuntimeException("Invalid Request!");
     }
 
 
-    // ======================================================================================
-    // MARK : Real Actions Section
+    // MARK : Real Actions Section ======================================================================================
 
 
+    /**
+     * the standard method that move designated pawn, it's the default move
+     * @param gameBoard the game board where the pawn have to move to
+     * @param designatedPawn the pawn that's designated to move
+     * @return the Consequence created by the move
+     */
     @Override
     public Consequence move(Board gameBoard, Pawn designatedPawn, Cell nextPosition) {
 
@@ -134,6 +184,14 @@ public class BasicEffect implements Effect {
     }
 
 
+    /**
+     * the standard method that build a building by the designated pawn, it's the default build
+     * @param designatedPawn the pawn that's designated to move
+     * @param designatedCell the game board where the pawn have to move to
+     * @param chosenLevel the level of the building
+     * @param buildings the list of the four buildings
+     * @return the Consequence created by the build
+     */
     @Override
     public Consequence build(Pawn designatedPawn, Cell designatedCell, int chosenLevel, List<Building> buildings) {
 
@@ -163,27 +221,33 @@ public class BasicEffect implements Effect {
     }
 
 
+    /**
+     * this method forces the pawn and return void because there is no consequence (victory or block) if you force a pawn
+     * @param designatedPawn the pawn forced
+     * @param nextPosition the cell where the pawn is forced to
+     */
     @Override
     public void force(Pawn designatedPawn, Cell nextPosition) {
         designatedPawn.forcePawn(nextPosition);
     }
 
 
-    // ======================================================================================
-    // MARK : Pawn Placing Section
+    /**
+     * this method is used only for the destroyBlockPlayer
+     */
+    /*@Override
+    public void destroy(Board gameBoard, Cell designatedCell) {}*/
 
 
-    @Override
-    public void removePawn(Board gameBoard, Pawn designatedPawn) {
-
-        int rowPosition = designatedPawn.getPosition().getRowPosition();
-        int columnPosition = designatedPawn.getPosition().getColumnPosition();
-
-        gameBoard.getCell(rowPosition, columnPosition).freeCell();
-
-    }
+    // MARK : Pawn Placing Section ======================================================================================
 
 
+    /**
+     * place the pawn on the board
+     * @param gameBoard the board where to place the pawn
+     * @param designatedPawn the pawn that we have to place
+     * @param designatedCell the cell where place the pawn
+     */
     @Override
     public void placePawn(Board gameBoard, Pawn designatedPawn, Cell designatedCell) {
 
@@ -191,6 +255,22 @@ public class BasicEffect implements Effect {
         int columnPosition = designatedCell.getColumnPosition();
 
         gameBoard.getCell(rowPosition, columnPosition).placePawnHere(designatedPawn);
+
+    }
+
+
+    /**
+     * remove a pawn from the board
+     * @param gameBoard we have to remove the pawn form this board
+     * @param designatedPawn the pawn to remove
+     */
+    @Override
+    public void removePawn(Board gameBoard, Pawn designatedPawn) {
+
+        int rowPosition = designatedPawn.getPosition().getRowPosition();
+        int columnPosition = designatedPawn.getPosition().getColumnPosition();
+
+        gameBoard.getCell(rowPosition, columnPosition).freeCell();
 
     }
 
--- src/main/java/it/polimi/ingsw/model/Player/Effect/Player.java
+++ src/main/java/it/polimi/ingsw/model/Player/Effect/Player.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+
 public class Player {
 
     private final int QUANTITY = 2;
@@ -56,6 +57,21 @@ public class Player {
     // ======================================================================================
 
 
+    public String getName() {
+        return this.name;
+    }
+
+
+    public Color getColor() {
+        return this.color;
+    }
+
+
+    public Card getCard() {
+        return this.card;
+    }
+
+
     public Effect getEffect() {
         return this.effect;
     }
@@ -74,52 +90,159 @@ public class Player {
     // ======================================================================================
 
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+
     public void setEffect(Effect effect) {
         this.effect = effect;
     }
 
 
+    /**
+     * this method resets the status of the player when his turn ends
+     */
+    public void resetPlayerStatus() {
+        for (Pawn p : pawns ) {
+            p.resetPawnStatus();
+        }
+    }
+
+
     // ======================================================================================
 
 
+    /**
+     * this player method only calls getPossibleAction of its effect
+     * @param gameBoard the game board
+     * @param designatedPawn the pawn that wants to know its actions
+     * @return the list of possible actions
+     */
     public List<Action> getPossibleActions(Board gameBoard, Pawn designatedPawn) {
         return effect.getPossibleActions(gameBoard,designatedPawn);
     }
 
 
+    /**
+     * this player method only calls wherePawnCanMove of its effect
+     * @param gameBoard the game board
+     * @param designatedPawn the pawn that I want to move
+     * @return the list of cells where the pawn can be moved
+     */
     public List<Cell> wherePawnCanMove (Board gameBoard, Pawn designatedPawn){
         return effect.wherePawnCanMove(gameBoard, designatedPawn);
     }
 
 
+    /**
+     * this player method only calls wherePawnCanBuild of its effect
+     * @param gameBoard the game board
+     * @param designatedPawn the pawn that wants to build
+     * @return the list of cells where the pawn can build
+     */
     public List<Cell> wherePawnCanBuild (Board gameBoard, Pawn designatedPawn) {
         return effect.wherePawnCanBuild(gameBoard, designatedPawn);
     }
 
 
+    /**
+     * this player method only calls getPossibleBuildingOnCell of its effect
+     * @param gameBoard the game board
+     * @param designatedCell the designated cell to be built on
+     * @return the list of possible buildings on this cell
+     */
     public List<Building> getPossibleBuildingOnCell(Board gameBoard, Cell designatedCell) {
         return effect.getPossibleBuildingOnCell(gameBoard, designatedCell);
     }
 
 
+    /**
+     * this player method only calls wherePawnCanForce of its effect
+     * @param gameBoard the game board
+     * @param designatedPawn the pawn that I want to force
+     * @return the list of cells
+     */
+    public List<Cell> wherePawnCanForce(Board gameBoard, Pawn designatedPawn) {
+        return this.effect.wherePawnCanForce(gameBoard, designatedPawn);
+    }
+
+
+    /**
+     * this player method only calls wherePawnCanDestroy of its effect
+     * @param gameBoard the game board
+     * @param designatedPawn the pawn that wants to destroy
+     * @return the list of cells
+     */
+    public List<Cell> wherePawnCanDestroy(Board gameBoard, Pawn designatedPawn) {
+        return this.effect.wherePawnCanDestroy(gameBoard, designatedPawn);
+    }
+
+
     // ======================================================================================
 
 
+    /**
+     * this method calls the move method of its effect and add the name of the player
+     * at the returning consequence
+     * @param gameBoard the game board
+     * @param designatedPawn the pawn that I'm moving
+     * @param nextPosition the next position of the pawn
+     * @return a {@link Consequence} based on the type of move
+     */
     public Consequence move(Board gameBoard, Pawn designatedPawn, Cell nextPosition) {
         Consequence consequence = effect.move(gameBoard,designatedPawn,nextPosition);
         consequence.setNickname(name);
+
         return consequence;
     }
 
 
+    /**
+     * this method calls the build method of its effect and add
+     * the name of the player ar the returning consequence
+     * @param designatedPawn the pawn that is building
+     * @param designatedCell the cell where the pawn is building
+     * @param chosenLevel the level of the chosen building
+     * @param buildings the list of all possible buildings
+     * @return a {@link Consequence} based on the type of build
+     */
     public Consequence build(Pawn designatedPawn, Cell designatedCell, int chosenLevel, List<Building> buildings) {
-        return effect.build(designatedPawn, designatedCell, chosenLevel, buildings);
+        Consequence consequence = effect.build(designatedPawn, designatedCell, chosenLevel, buildings);
+        consequence.setNickname(name);
+
+        return consequence;
+    }
+
+
+    /**
+     * this method only calls the force of its effect
+     * @param designatedPawn the pawn that I'm forcing
+     * @param designatedCell its next cell
+     */
+    public void force(Pawn designatedPawn, Cell designatedCell) {
+        this.effect.force(designatedPawn, designatedCell);
     }
 
 
     // ======================================================================================
 
 
+    /**
+     * this method initiate one male pawn and one female pawn and place them on the board
+     * @param gameBoard the board where we have to place the pawns
+     * @param startCell the cell where to place the pawn initiated
+     */
     public void initPawn(Board gameBoard, Cell startCell ) {
 
         if(pawns.size() == 0) {
@@ -134,6 +257,12 @@ public class Player {
     }
 
 
+    /**
+     * place the pawn on the board
+     * @param gameBoard the board where to place the pawn
+     * @param designatedPawn the pawn that we have to place
+     * @param designatedCell the cell where place the pawn
+     */
     public void placePawn(Board gameBoard, Pawn designatedPawn, Cell designatedCell) {
 
         int rowPosition = designatedCell.getRowPosition();
@@ -143,6 +272,19 @@ public class Player {
 
     }
 
+
+    /**
+     * remove a pawn from the board
+     * @param gameBoard we have to remove the pawn form this board
+     * @param designatedPawn the pawn to remove
+     */
+    public void removePawn(Board gameBoard, Pawn designatedPawn) {
+
+        int rowPosition = designatedPawn.getPosition().getRowPosition();
+        int columnPosition = designatedPawn.getPosition().getColumnPosition();
+
+        gameBoard.getCell(rowPosition, columnPosition).freeCell();
+    }
 
 
 }
